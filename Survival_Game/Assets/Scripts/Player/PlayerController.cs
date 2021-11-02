f12ea522d8708410aa4042f7a60cb15026b3ca1b
lukigu <82062009+lukigu@users.noreply.github.com>
2021-11-02 14:47:43 +0100

    Camera movement

--- Survival_Game/Assets/Scripts/Player/PlayerController.cs
+++ Survival_Game/Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Transform cameraContainer;
+    public float minXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensivity;
+
+    private Vector2 mouseDelta;
+
     void Start()
     {
-        
+        // lock the cursor at the start of the game
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        CameraLook();
+    }
+
+    void CameraLook()
+    {
+        // rotate the camera container up and down
+        camCurXRot += mouseDelta.y * lookSensivity;
+        camCurXRot = Mathf.Clamp(camCurXRot, minXLook, maxXLook);
+        cameraContainer.localEulerAngles = new Vector3(-camCurXRot, 0, 0);
+
+        // rotate the player left and right
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensivity, 0);
+    }
+
+    // called when we move our mouse - managed by the Input System
+    public void OnLookInput(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
     }
 }
