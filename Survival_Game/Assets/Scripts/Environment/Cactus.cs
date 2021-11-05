a83cf652973b577f37bb45274268eb96308118b1
lukigu <82062009+lukigu@users.noreply.github.com>
2021-11-05 15:13:44 +0100

    Cactus damage player

--- Survival_Game/Assets/Scripts/Environment/Cactus.cs
+++ Survival_Game/Assets/Scripts/Environment/Cactus.cs
@@ -9,6 +9,11 @@ public class Cactus : MonoBehaviour
 
     private List<IDamagable> thingsToDamage = new List<IDamagable>();
 
+    private void Start()
+    {
+        StartCoroutine(DealDamage());
+    }
+
     IEnumerator DealDamage()
     {
         // every "damageRate" seconds, damage all thingsToDamage
@@ -20,6 +25,26 @@ public class Cactus : MonoBehaviour
             }
 
             yield return new WaitForSeconds(damageRate);
+        }
+    }
+
+    // called when an object collides with the cactus
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if it's an IDamagable, add it to the list
+        if (collision.gameObject.GetComponent<IDamagable>() != null)
+        {
+            thingsToDamage.Add(collision.gameObject.GetComponent<IDamagable>());
+        }
+    }
+
+    // called when an object stops colliding with the cactus
+    private void OnCollisionExit(Collision collision)
+    {
+        // if it's an IDamagable, remove it from the list
+        if (collision.gameObject.GetComponent<IDamagable>() != null)
+        {
+            thingsToDamage.Remove(collision.gameObject.GetComponent<IDamagable>());
         }
     }
 }
