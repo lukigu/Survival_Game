66a80ac333e75ca53e60f7c1b7256a2c82e0b67e
lukigu <82062009+lukigu@users.noreply.github.com>
2021-11-08 13:15:00 +0100

    Day night cycle part one

---
+++ Survival_Game/Assets/Scripts/Environment/DayNightCycle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [Range(0.0f, 1.0f)]
+    public float time;
+    public float fullDayLength;
+    public float startTime = 0.4f;
+    private float timeRate;
+    public Vector3 noon;
+
+    [Header("Sun")]
+    public Light sun;
+    public Gradient sunColor;
+    public AnimationCurve sunIntensity;
+}
