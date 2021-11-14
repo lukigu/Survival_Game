efbcd64a74c2dc548015e222089b954f8219a403
lukigu <82062009+lukigu@users.noreply.github.com>
2021-11-14 21:53:21 +0100

    Adding items to inventory

--- Survival_Game/Assets/Scripts/Player/Inventory.cs
+++ Survival_Game/Assets/Scripts/Player/Inventory.cs
@@ -58,6 +58,131 @@ public class Inventory : MonoBehaviour
             uiSlots[x].Clear();
         }
     }
+
+    // opens or closes the inventory
+    public void Toggle()
+    {
+
+    }
+
+    // is the inventory currently open?
+    public bool Isopen()
+    {
+        return inventoryWindow.activeInHierarchy;
+    }
+
+    // adds the requested item to the player's inventory
+    public void AddItem(ItemData item)
+    {
+        if(item.canStack)
+        {
+            ItemSlot slotToStackTo = GetItemStack(item);
+
+            if(slotToStackTo != null)
+            {
+                slotToStackTo.quantity++;
+                UpdateUI();
+                return;
+            }
+        }
+
+        ItemSlot emptySlot = GetEmptySlot();
+
+        if(emptySlot != null)
+        {
+            emptySlot.item = item;
+            emptySlot.quantity = 1;
+            UpdateUI();
+            return;
+        }
+
+        ThrowItem(item);
+    }
+
+    // spawns the item infront of the player
+    void ThrowItem(ItemData item)
+    {
+
+    }
+
+    // updates the UI slots
+    void UpdateUI()
+    {
+
+    }
+
+    // returns the item slot that the requested item can be stacked on
+    // returns null if there is no stack available
+    ItemSlot GetItemStack(ItemData item)
+    {
+        return null;
+    }
+
+    // returns an empty slot in the inventory
+    // if there are no empty slots - return null
+    ItemSlot GetEmptySlot()
+    {
+        return null;
+    }
+
+    // called when we click on an item slot
+    public void SelectItem(int index)
+    {
+
+    }
+
+    // called when the inventory opens or the currently selected item has depleted
+    void ClearSelectedItemWindow()
+    {
+
+    }
+
+    // called when the "Use" button is pressed
+    public void OnUseButton()
+    {
+
+    }
+
+    // called when the "Equip" button is pressed
+    public void OnEquipButton()
+    {
+
+    }
+
+    // unequips the requested item
+    void UnEquip(int inedex)
+    {
+
+    }
+
+    // called when the "UnEquip" button is pressed
+    public void OnUnEquipButton()
+    {
+
+    }
+
+    // called when the "Drop" button is pressed
+    public void OnDropButton()
+    {
+
+    }
+
+    // removes the currently selected item
+    void RemoveSelectedItem()
+    {
+
+    }
+
+    public void RemoveItem(ItemData item)
+    {
+
+    }
+
+    // does the player have "quantity" amount of "item"s? later for crafting
+    public bool HasItems(ItemData item, int quantity)
+    {
+        return false;
+    }
 }
 
 public class ItemSlot
