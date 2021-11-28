13b86d7e65ccbd9508c45ef0c39b371bdfb8fb70
lukigu <82062009+lukigu@users.noreply.github.com>
2021-11-28 18:26:23 +0100

    Equiptool script

--- Survival_Game/Assets/Scripts/Player/EquipTool.cs
+++ Survival_Game/Assets/Scripts/Player/EquipTool.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EquipTool : Equip
 {
-    // Start is called before the first frame update
-    void Start()
+    public float attackRate;
+    private bool attacking;
+    public float attackDistance;
+
+    [Header("Resource Gathering")]
+    public bool doesGatherResources;
+
+    [Header("Combat")]
+    public bool doesDealDamage;
+    public int damage;
+
+    //components
+    private Animator anim;
+    private Camera cam;
+
+    private void Awake()
     {
-        
+        //get our components
+        anim = GetComponent<Animator>();
+        cam = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnAltAttackInput()
     {
-        
+        if(!attacking)
+        {
+            attacking = true;
+            anim.SetTrigger("Attack");
+            Invoke("OnCanAttack", attackRate);
+        }
+    }
+
+    void OnCanAttack()
+    {
+        attacking = true;
+    }
+
+    public void OnHit()
+    {
+
     }
 }
