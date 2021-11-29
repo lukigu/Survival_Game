32a095a3088c8537bc04f41d026b42ca0e0b4a30
lukigu <82062009+lukigu@users.noreply.github.com>
2021-11-29 23:25:19 +0100

    Crafting window script

--- Survival_Game/Assets/Scripts/Crafting/CraftingRecipeUI.cs
+++ Survival_Game/Assets/Scripts/Crafting/CraftingRecipeUI.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CraftingRecipeUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CraftingRecipe recipe;
+    public Image backgroundImage;
+    public Image icon;
+    public TextMeshProUGUI itemName;
+    public Image[] resourceCosts;
 
-    // Update is called once per frame
-    void Update()
+    public Color canCraftColor;
+    public Color cannotCraftColor;
+    private bool canCraft;
+
+    public void UpdateCanCraft()
     {
-        
+        canCraft = true;
+
+        for(int i = 0; i < recipe.cost.Length; i++)
+        {
+            if(Inventory.instance.HasItems(recipe.cost[i].item, recipe.cost[i].quantity))
+            {
+                canCraft = false;
+                break;
+            }
+        }
+
+        backgroundImage.color = canCraft ? canCraftColor : cannotCraftColor;
     }
 }
--- Survival_Game/Assets/Scripts/Crafting/CraftingWindow.cs
+++ Survival_Game/Assets/Scripts/Crafting/CraftingWindow.cs
@@ -30,6 +30,19 @@ public class CraftingWindow : MonoBehaviour
 
     public void Craft(CraftingRecipe recipe)
     {
+        for(int i = 0; i < recipe.cost.Length; i++)
+        {
+            for(int x = 0; x < recipe.cost[i].quantity; x++)
+            {
+                Inventory.instance.RemoveItem(recipe.cost[i].item);
+            }
+        }
 
+        Inventory.instance.AddItem(recipe.itemToCraft);
+
+        for(int i = 0; i < recipeUIs.Length; i++)
+        {
+            recipeUIs[i].UpdateCanCraft();
+        }
     }
 }
